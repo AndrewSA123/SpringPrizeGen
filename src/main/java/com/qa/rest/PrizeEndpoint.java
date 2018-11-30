f9203ba5dcf22e5445871cf07e34141400c621d7
andrewsa123 <andrewknott_123@hotmail.co.uk>
2018-11-30 13:26:42 +0000

    CORS

--- src/main/java/com/qa/rest/PrizeEndpoint.java
+++ src/main/java/com/qa/rest/PrizeEndpoint.java
@@ -1,6 +1,7 @@
 package com.qa.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.qa.service.IPrizeService;
 
 @RestController
 @RequestMapping("${base_endpoint}")
+@CrossOrigin
 public class PrizeEndpoint {
 
 	@Autowired
